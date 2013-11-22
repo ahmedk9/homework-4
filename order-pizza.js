fba17caeabe05a45ffec86134c41bee0ff603f58
Ahmed Elkholy <ahmedk9@uw.edu>
2013-11-21 19:49:44 -0800

    iterm results 4 for homework 4

--- order-pizza.js
+++ order-pizza.js
@@ -16,9 +16,9 @@ function pizza(){
         clonedTemplate.find(".description").html(pizza.description);
 
 
-        clonedTemplate.find(".small").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "small" data-price = "'+ pizza.prices[0] + '">' + "Small $" + pizza.prices[0] + '</button>' + " " );
-        clonedTemplate.find(".medium").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "medium" data-price = "'+ pizza.prices[1] + '">' + "Medium $" + pizza.prices[1] + '</button>' + " " );
-        clonedTemplate.find(".large").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "large" data-price = "'+ pizza.prices[2] + '">' + "Large $" + pizza.prices[2] + '</button>' + " ");
+        clonedTemplate.find(".small").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "Small" data-price = "'+ pizza.prices[0] + '">' + "Small $" + pizza.prices[0] + '</button>' + " " );
+        clonedTemplate.find(".medium").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "Medium" data-price = "'+ pizza.prices[1] + '">' + "Medium $" + pizza.prices[1] + '</button>' + " " );
+        clonedTemplate.find(".large").html('<button type= "button" class="btn btn-primary btn-xs" data-type = "pizza" data-name = "'+ pizza.name + '" data-size = "Large" data-price = "'+ pizza.prices[2] + '">' + "Large $" + pizza.prices[2] + '</button>' + " ");
 
 
         clonedTemplate.removeClass("template-pizzas");
@@ -33,7 +33,7 @@ function pizza(){
 }
 
 function addToCart() {
-   // $(".submit-order-btn").show();
+    $(".submit-order-btn").show();
 
     var name = $(this).data("name");
     var type = $(this).data("type");
@@ -53,7 +53,7 @@ function addToCart() {
     if (cart.existsInCart(item) == -1) {
         cart.addItem(item);
         var itemHtml = $(".template-cart").clone().removeClass("template-cart");
-        itemHtml.html("1x " + size + " " + name + "<span class=\"glyphicon glyphicon-remove\"></span>");
+        itemHtml.html("1x $" + price + " " + size + " " + name + "<span class=\"glyphicon glyphicon-remove\"></span>");
         itemHtml.attr("data-name", name);
         itemHtml.attr("data-type", type);
         itemHtml.attr("data-size", size);
@@ -65,12 +65,12 @@ function addToCart() {
     } else {
         cart.addItem(item);
         var itemHtml = $('.cart-item[data-name="' + name + '"].cart-item[data-size="' + size + '"]');
-        itemHtml.html(cart.getQuantity(item) + 1 + "x " + size + " " + name + "<span class=\"glyphicon glyphicon-remove\"></span>");
+        itemHtml.html(cart.getQuantity(item) + 1 + "x $" +price +" " + size + " " + name + "<span class=\"glyphicon glyphicon-remove\"></span>");
         itemHtml.attr("data-quantity", cart.getQuantity(item) + 1);
     }
 
     total += price;
-    $(".cart-price").html( "Total: $ " +total);
+    $(".cart-price").html("Total: " + "$" + total + " + $" + (total * 0.095).toFixed(2) + "(Tax) = " + (total * 1.095).toFixed(2));
     
 }
 function removeFromCart() {
@@ -90,13 +90,13 @@ function removeFromCart() {
         });
 
         total -= price * (cart.getQuantity(item) + 1);
-        $(".cart-price").html("Total: $" + total);
+    $(".cart-price").html("Total: " + "$" + total + " + $" + (total * 0.095).toFixed(2) + "(Tax) = " + (total * 1.095).toFixed(2));
 
         cart.removeItem(item);
         $(this).remove();
 
-        if (total == 0) {
-            $(".submit-order-btn").hide();
+        if( total == 0) {
+            $(".cart-price").html("Total: $0 ");
         }
     }
 
@@ -147,5 +147,13 @@ $(function(){
     drinks();
     desserts(); 
     $(".btn-xs").click(addToCart);
+    $(".submit-order-btn").click(function(){
+         if (total >= 20) {
+            $("#submitOrderForm").modal();
+            $(".minimum").html("");
+        } else {
+            $(".minimum").html("To deliver, the total should be at least $20.");
+        }
+    });
  })
 
